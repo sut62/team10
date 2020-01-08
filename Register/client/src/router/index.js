fb9e93989a4cf123708bb488fd0be1c3d612fe07
BBieSasithon <sasithonbebie@gmail.com>
2020-01-08 22:11:51 +0700

    ทำการเพิ่ม client - close #55

---
+++ Register/client/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Register from '../components/Register.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/Register',
+    component: Register
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
