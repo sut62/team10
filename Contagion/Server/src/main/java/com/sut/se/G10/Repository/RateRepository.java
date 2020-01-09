b8823a9be3a5c2044e120154ac993d2d0e7738a0
Pongsiri Phadungkhant <boss123440@hotmail.com>
2020-01-10 04:54:41 +0700

    Close #67 - delete trash file

--- Contagion/Server/src/main/java/com/sut/se/G10/Repository/RateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sut.se.G10.Repository; 
-import com.sut.se.G10.Entity.Rate; 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;    
-@RepositoryRestResource 
-public interface RateRepository extends JpaRepository<Rate, Long> {
-    Rate findById(long rateid);
-}
--- sunita/repository/ContagionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sut.se.G10.Repository; 
-import com.sut.se.G10.Entity.Contagion; 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;    
-@RepositoryRestResource 
-public 
-interface ContagionRepository extends JpaRepository<Contagion, Long> {
-}
