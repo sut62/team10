5891176e9589d7e3925d574135e974e5e64db50f
Nawapat Suengprom <pleum.pleum.8919@gmail.com>
2020-01-10 19:03:58 +0700

    ปิดคอมเม้นใน MedicalStaffRepository.java, MedicalStaffController.java และ PositionRepository.java #104

--- ContagiousDiseaseManagementSystem/server/src/main/java/com/sut/se/G10/Register/Controller/MedicalStaffController.java
+++ ContagiousDiseaseManagementSystem/server/src/main/java/com/sut/se/G10/Register/Controller/MedicalStaffController.java
@@ -64,12 +64,12 @@ public class MedicalStaffController {
         return medicalStaffRepository.findById(id);
     }
 
-    @GetMapping("/medicalStaffWherePositionIsDoctor")
+    /*@GetMapping("/medicalStaffWherePositionIsDoctor")
     public Collection<MedicalStaff> getMedicalStaffWherePositionisDoctors() {
         long id;
         id = positionRepository.findByPosition("Doctor").getId();
         return medicalStaffRepository.findByMedicalStaffId(id);
-    }
+    }*/
 
     @PostMapping("/medicalstaff/{gender_id}/{position_id}/{province_id}/{address}/{fullname}/{email}/{phone}/{password}/{bdate}")
     public MedicalStaff newMedicalStaff(  MedicalStaff newMedicalStaff, 
