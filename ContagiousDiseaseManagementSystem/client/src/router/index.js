70394a3f2dab25368347ea36879fffe6dfc9e07d
Saratoon Nampan <Saratoon892020.sn@gmail.com>
2020-01-10 09:18:37 +0700

    close #88 -เพิ่ม index

--- ContagiousDiseaseManagementSystem/client/src/router/index.js
+++ ContagiousDiseaseManagementSystem/client/src/router/index.js
@@ -1,21 +1,54 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import router from 'vue-router'
+import Home from '../views/Home.vue'
+import Contagion from '../components/Contagion.vue'
+import Diagnose from '../components/Diagnose.vue'
+import Patient from '../components/Patient.vue'
+import Register from '../components/Register.vue'
 import Riskarea from '../components/Riskarea.vue'
+import vaccineinformation from '../components/vaccineinformation.vue'
+import viewvaccineinformation from '../views/viewvaccineinformation.vue'
 
-Vue.use(VueRouter)
+Vue.use(router);
 
-const routes = [
+export default new router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+ 
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/contagion',
+    component: Contagion
+  },
+  {
+    path: '/diagnose',
+    component: Diagnose
+  },
+  {
+    path: '/patient',
+    component: Patient
+  },
+  {
+    path: '/register',
+    component: Register
+  },
   {
     path: '/riskarea',
-    name: 'riskarea',
     component: Riskarea
+  },
+  {
+    path: '/vaccineinformation',
+    component: vaccineinformation
+  },
+  {
+    path: '/viewvaccineinformation',
+    component: viewvaccineinformation
   }
 ]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
 })
 
-export default router
+
+
