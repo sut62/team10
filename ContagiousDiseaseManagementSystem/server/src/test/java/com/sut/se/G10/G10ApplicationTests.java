c95d4b38257008e1fef173f8d5c297e4a03f632a
Pongsiri Phadungkhant <boss123440@hotmail.com>
2020-01-15 12:03:20 +0700

    Close #113 - modify G10applicationtest.java and build.gradlew

--- ContagiousDiseaseManagementSystem/server/src/test/java/com/sut/se/G10/G10ApplicationTests.java
+++ ContagiousDiseaseManagementSystem/server/src/test/java/com/sut/se/G10/G10ApplicationTests.java
@@ -1,12 +1,10 @@
 package com.sut.se.G10;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class G10ApplicationTests {
 
 	@Test
