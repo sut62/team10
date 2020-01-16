eccfb684940bc09050c021c2e0b52c29fc50bb22
Pongsiri Phadungkhant <boss123440@hotmail.com>
2020-01-16 09:05:22 +0700

    Close #126 - chang import in G10Application.java

--- ContagiousDiseaseManagementSystem/server/src/test/java/com/sut/se/G10/G10ApplicationTests.java
+++ ContagiousDiseaseManagementSystem/server/src/test/java/com/sut/se/G10/G10ApplicationTests.java
@@ -1,11 +1,12 @@
 package com.sut.se.G10;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 public class G10ApplicationTests {
 
-	@org.junit.jupiter.api.Test
+	@Test
 	public void contextLoads() {
 	}
 
