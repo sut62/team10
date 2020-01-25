d097466cfec27463f978f8394f7f6a1fc451adc1
Saratoon Nampan <Saratoon892020.sn@gmail.com>
2020-01-25 22:58:36 +0700

    close #195 เพิ่ม import alert ใหม่

--- ContagiousDiseaseManagementSystem/client/src/main.js
+++ ContagiousDiseaseManagementSystem/client/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 import router from './router'
+import 'v-slim-dialog/dist/v-slim-dialog.css'
+import SlimDialog from 'v-slim-dialog';
 
 Vue.config.productionTip = false
+Vue.use(SlimDialog)
 
 new Vue({
   vuetify,
