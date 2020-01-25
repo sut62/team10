59505f56c640031a167940bc4c1ce00b7df95831
Saratoon Nampan <Saratoon892020.sn@gmail.com>
2020-01-25 19:20:31 +0700

    close #189 - แก้ไขเพิ่ม UI viewDataVaccine.vue

--- ContagiousDiseaseManagementSystem/client/src/components/sprint2/viewDataVaccine.vue
+++ ContagiousDiseaseManagementSystem/client/src/components/sprint2/viewDataVaccine.vue
@@ -1,54 +1,48 @@
 <template>
- <div class="text-center">
-  <v-container>
-    <v-card class="mx-auto text-center" style="width: 950px;">
-      <!-- หัวเรื่อง -->
-      <v-layout text-center wrap>
-        <v-flex mb-4>
-          <br />
-          <h1 class="display-2 font-weight-bold mb-3">แสดงข้อมูลวัคซีนป้องกัน/ยา</h1>
-        </v-flex>
-      </v-layout>
+  <div class="text-center">
+    <v-container>
+      <v-card class="mx-auto text-center" style="width: 1000px;">
+        <!-- หัวเรื่อง -->
+        <v-layout text-center wrap>
+          <v-flex mb-4>
+            <br />
+            <h1 class="display-2 font-weight-bold mb-3">แสดงข้อมูลวัคซีนป้องกัน/ยา</h1>
+          </v-flex>
+        </v-layout>
 
-      <!-- ค้นหาด้วย vaccine -->
-      <v-row >
-        <v-spacer></v-spacer>
-        <v-col cols ="6">
-          <v-select
-            v-model="vaccineinformation.vaccineid"
-            :items="vaccines"
-            item-text="vaccinename"
-            item-value="vaccineid"
-            :rules="[v => !!v || 'Item is required']"
-            label="-- Vaccine --"
-            required
-          ></v-select>
-        </v-col>
-        <v-col> 
-            <v-btn color="success" height="40" width="100" @click="findbyData">ค้นหา</v-btn>
-        </v-col>
-      </v-row>
-          <!-- แสดงผลข้างเคียงของวัคซีนป้องกัน/ยา -->
-      <v-row  v-if="check">
-          <v-col cols="11">
+        <v-row justify="center">
+          <v-col cols="5">
+            <v-text-field
+              v-model="search"
+              label="ค้นหา"
+              prepend-icon="mdi mdi-file-find"
+              outlined
+              hide-details
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="15">
             <v-data-table
               :headers="headers"
-              :items="vcdata"
+              :items="vaccines"
               :items-per-page="5"
+              :search="search"
               class="elevation-1"
             ></v-data-table>
           </v-col>
-      </v-row>
+        </v-row>
 
-      <!-- ปุ่มกลับ -->
-      <v-row justify="center">
-        <v-col cols="2">
-          <v-btn color="success" height="40" width="100" to="/vaccineinformation">กลับ</v-btn>
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-container>
- </div>
+        <!-- ปุ่มกลับ -->
+        <v-row justify="center">
+          <v-col cols="2">
+            <v-btn color="success" height="40" width="100" to="/vaccineinformation">กลับ</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -56,26 +50,21 @@ import http from "../sprint2/http-common";
 export default {
   data() {
     return {
-      check: false,
-      vaccineid: [
-      ],
-      vaccines: [],
-      vaccinename: '',
-      vcdata:[{
-            vaccinedata: ""
-        }],
-      vaccineinformation: [],
-      headers: [
+      search: "",
+      vaccines: [
         {
-          text: "ผลข้างเคียงของวัคซีนป้องกัน/ยา",
-          value: "vaccine.vaccinedata"
+          vaccinedata: ""
         }
+      ],
+      headers: [
+        { text: "ขื่อวัคซีนป้องกัน/ยา", value: "vaccinename" },
+        { text: "ผลข้างเคียงของวัคซีนป้องกัน/ยา", value: "vaccinedata" }
       ]
     };
   },
   /* eslint-disable no-console */
   methods: {
-      findbyData() {
+    findbyData() {
       http
         .get("/vaccine/" + this.vaccineinformation.vaccineid)
         .then(response => {
@@ -83,10 +72,8 @@ export default {
           console.log(response);
           if (response.data != null) {
             this.check = true;
-          } else {
-            alert("ไม่สามารถค้นหาได้");
           }
-        })
+        });
     },
 
     getVaccines() {
@@ -100,6 +87,7 @@ export default {
           console.log(e);
         });
     },
+
     pushvaccineinformation() {
       this.$router.push("/vaccineinformation");
     }
