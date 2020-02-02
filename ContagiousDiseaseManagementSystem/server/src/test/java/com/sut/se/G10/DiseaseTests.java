b0e8848671a691f76a6073f879f5c5df53de2471
sunita lomkrathok <jayneszee@gmail.com>
2020-02-02 22:14:57 +0700

    close #225 -modify DiseaseTests.java

--- ContagiousDiseaseManagementSystem/server/src/test/java/com/sut/se/G10/DiseaseTests.java
+++ ContagiousDiseaseManagementSystem/server/src/test/java/com/sut/se/G10/DiseaseTests.java
@@ -7,21 +7,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
 import java.util.Optional;
 import java.util.Set;
-import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 @DataJpaTest
 public class DiseaseTests {
@@ -50,6 +45,34 @@ public class DiseaseTests {
 
     @Test
     void b5911837_testDiseaseNotNull() {
+        Disease disease = new  Disease();
+        disease.setDisease(null);
+
+        Set<ConstraintViolation<Disease>> result = validator.validate(disease);
+
+        // result ต้องมี error 1 ค่าเท่านั้น
+        assertEquals(1, result.size());
+
+        // error message ตรงชนิด และถูก field
+        ConstraintViolation<Disease> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("disease", v.getPropertyPath().toString());
+    }
+
+    //============================B5913480============================
+    @Test
+    void b5913480_testDiseaseAllCorrect() {
+        Disease disease = new  Disease();
+        disease.setDisease("ตาเเดง");
+
+        disease =  diseaseRepository.saveAndFlush(disease);
+
+        Optional<Disease> found = diseaseRepository.findById(disease.getId());
+        assertEquals("ตาเเดง", found.get().getDisease());
+    }
+
+    @Test
+    void b5913480_testDiseaseNotNull() {
         Disease disease = new  Disease();
         disease.setDisease(null);
 
