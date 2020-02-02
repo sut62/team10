25014e80e107bfa7c28cb48d629de01d5ba7a68e
sunita lomkrathok <jayneszee@gmail.com>
2020-02-02 22:00:27 +0700

    close #224 -เพิ่ม test case Typetest.java and Provincetest.java

---
+++ ContagiousDiseaseManagementSystem/server/src/test/java/com/sut/se/G10/ProvinceTest.java
@@ -0,0 +1,62 @@
+package com.sut.se.G10;
+
+import com.sut.se.G10.Register.Entity.Province;
+import com.sut.se.G10.Register.Repository.ProvinceRepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class ProvinceTest {
+
+    private Validator validator;
+
+    @Autowired
+    private ProvinceRepository provinceRepository;
+
+    @BeforeEach
+    public void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void b5913480_testProvinceAllCorrect() {
+        Province province = new Province();
+        province.setProvince("กรุงเทพมหานคร");
+
+        province =  provinceRepository.saveAndFlush(province);
+
+        Optional<Province> found = provinceRepository.findById(province.getId());
+        assertEquals("กรุงเทพมหานคร", found.get().getProvince());
+    }
+
+    @Test
+    void b5913480_testProvinceNotNull() {
+        Province province = new Province();
+        province.setProvince(null);
+
+        Set<ConstraintViolation<Province>> result = validator.validate(province);
+
+        // result ต้องมี error 1 ค่าเท่านั้น
+        assertEquals(1, result.size());
+
+        // error message ตรงชนิด และถูก field
+        ConstraintViolation<Province> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("province", v.getPropertyPath().toString());
+    }
+
+}
