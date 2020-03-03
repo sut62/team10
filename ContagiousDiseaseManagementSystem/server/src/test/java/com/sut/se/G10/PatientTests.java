a31c9cb6ade0f599e57e5a96a65f709185358116
Wachiraya Chaiyasaj <chichiwaci@gmail.com>
2020-03-03 14:45:54 +0700

    แก้ไข PatientTests - close#246

--- ContagiousDiseaseManagementSystem/server/src/test/java/com/sut/se/G10/PatientTests.java
+++ ContagiousDiseaseManagementSystem/server/src/test/java/com/sut/se/G10/PatientTests.java
@@ -6,25 +6,21 @@ import com.sut.se.G10.Patient.Entity.Bloodtype;
 import com.sut.se.G10.Patient.Repository.PatientRepository;
 import com.sut.se.G10.Register.Repository.GenderRepository;
 import com.sut.se.G10.Patient.Repository.BloodtypeRepository;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
-
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Optional;
 import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
